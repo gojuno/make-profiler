c5537e29357aae716f406578e0272047f79f2511
Darafei Praliaskouski <komzpa@gojuno.com>
2017-01-19 14:57:58 +0300

    Document and boast all features

--- README.md
+++ README.md
@@ -1,23 +1,36 @@
-    sudo apt install python3-pip graphviz
-    pip3 install more_itertools
+#Makefile Profiler
 
-Example of usage:
+Helps managing a large data processing pipeline written in Makefile.
 
-    ./preprocess.py -i Makefile_template -o Makefile
-    ./clean.py -i Makefile_template any_targets you_like
-    make -j -k
-    ./dot_export.py -i Makefile_template
-    xdg-open make.png
-
-Features:
+##Features
 
 * SVG build overview;
 
-* inlining of images into build overview;
+* Inline pictures-targets into build overview;
 
 * Logs for each target marked with timestamps;
 
-* Easily distinguish a failed target execution from failed touch;
+* Distinguish a failed target execution from forgotten touch;
+
+* Navigate to last run's logs from each target directly from call graph;
 
 * Support for self-documented Makefiles according to 
-http://marmelab.com/blog/2016/02/29/auto-documented-makefile.html;
+http://marmelab.com/blog/2016/02/29/auto-documented-makefile.html
+
+##Sample overview
+![](make.svg)
+
+##Example usage
+
+    pip install make-profiler
+
+    cd your_project
+    make_profiler -h                # have a look at help
+
+    make_profiler                   # generate overview graph without profiling data
+    xdg-open make.svg               # have a look at call graph
+
+    make_profiler_clean target_to_remove_with_children
+
+    make_profiler -j -k target_name # run some target, record execution times and logs
+    xdg-open make.svg               # have a look at call graph with timing data
--- test/example.mk
+++ test/example.mk
@@ -2,41 +2,61 @@
 VAR1:='value'
 VAR2=$(shell pwd)
 
-NAME='some_name'
+NAME=some_name
 
 all: \
 	target1 \
-	target2 \
-	unknown \
-	fail \
-	target3 | target4 target5
+	forgot_to_implement \
+	fails \
+	target3 | target4 target_order_only_directory ## Order only dependencies usually just clutter the graph so not rendered
+	touch $@
 
+not_all: target1 target2 cat_graph.png ## Second result target, builds just a part of pipeline
 
-$(NAME):
+$(NAME): ## We don't support variable substitution - names rendered as is and no color inferred
 	sleep 1
+	touch $@
 
-target1: target11 target12 target13
+target1: target11 target12 target13 forgot_to_touch
 ifeq ($(BUILD_TYPE),QA)
 	echo 'target1: qa build'
 else
 	echo 'target1: build'
 endif
+	touch $@
 
 target11:
 	sleep 1
+	touch $@
 
 target12:
 	sleep 1
+	touch $@
 
-target13: target11
+cat_graph.png: target2 ## If your target generates visual content, it will be embedded into graph
+	wget https://laurensmemory.files.wordpress.com/2011/05/20110526-092336.jpg -O $@
+
+target13: target11 ## Sibling dependency 13 -> 11 is non-essential, thus rendered in grey
 	sleep 2
+	touch $@
 
+forgot_to_touch: target11 ## Forgot to touch the file, visible in graph
+	sleep 1
+
+tool_target: ## Tool targets are the ones that don't depend on anything and nobody depends on them. Think 'clean'
+	sleep 1
+	touch $@
+
+clean: ## Clean up things from previuous runs
+	make_profile_clean target_order_only_1
 
 target2:
 	sleep 2
+	touch $@
 
 target3: $(NAME)
 	sleep 3
+	touch $@
 
 target4:
 	for num in '5' '1' '3' '4' '5' '2' '4' ; do \
@@ -44,13 +64,10 @@ target4:
 	done \
 		| sort | uniq
 	sleep 5
+	touch $@
 
-target5:
-	sleep 5
+target_order_only_directory: ## Order only target should just be present. Useful for directories
+	mkdir -p $@
 
-
-unknown:
-	unknown
-
-fail:
-	exit 2
+fails: target11 ## This target fails, thus rendered in red
+	sh -c 'exit 1'
