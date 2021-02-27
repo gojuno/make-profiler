10e6a44c4338fbea675e21058b092347b636b76b
trylinka <tatyanakarvat@gmail.com>
2021-02-27 22:49:26 +0300

    Add lint_makefile into make_profiler

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
         'console_scripts': [
             'profile_make_clean = make_profiler.cmd_clean:main',
             'profile_make = make_profiler.__main__:main',
-            'profile_make_lint = lint_makefile:main'
+            'profile_make_lint = make_profiler.lint_makefile:main'
         ]
     },
     license='BSD',
