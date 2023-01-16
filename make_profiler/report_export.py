82ee16373f02788023fe7bfa0619fa444f69c297
isaricicek <isaricicek@kontur.io>
2023-01-16 13:30:21 +0300

    14020 write json instead of json string

--- make_profiler/report_export.py
+++ make_profiler/report_export.py
@@ -1,28 +1,20 @@
-#!/usr/bin/python3
-
 from datetime import datetime
+import json
 DATE_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
 
+status_list = {}
+status = []
 
-def escape_json_string(s):
-    s=str(s)
-    s = s.replace("\"", "")
-    s = s.replace("'","")
-    s = s.replace("\\","\\\\") # single \ to double \\
-    return s
-
-
-def export_report(performance,docs):
+def export_report(performance, docs):
     strOutputFile = 'report.json'
     fo = open(strOutputFile, 'w', encoding="utf-8")
-    n_in_progress=0
-    n_failed=0
+    n_in_progress = 0
+    n_failed = 0
     n_total = 0
     oldest_completed_target = ''
-    fo.write('{\n')
-    fo.write('    "status":[\n')
+
     for key in performance:
-        rec=performance[key]
+        rec = performance[key]
         n_total += 1
         if rec["failed"] == True:
             event_type = "failed"
@@ -37,56 +29,55 @@ def export_report(performance,docs):
 
         #event_time = datetime.fromtimestamp(int(rec['start_current'])).strftime(DATE_FORMAT)
         if 'finish_prev' in rec:
-            last_event_time = datetime.fromtimestamp(int(rec['finish_prev'])).strftime(DATE_FORMAT)
+            last_event_time = datetime.fromtimestamp(
+                int(rec['finish_prev'])).strftime(DATE_FORMAT)
         else:
             last_event_time = ''
 
         if 'start_current' in rec:
-            event_time = datetime.fromtimestamp(int(rec['finish_current'])).strftime(DATE_FORMAT)
+            event_time = datetime.fromtimestamp(
+                int(rec['finish_current'])).strftime(DATE_FORMAT)
         else:
             event_time = last_event_time
 
         if event_type == "completed":
             if oldest_completed_target == '':
                 oldest_completed_target = event_time
-            if event_time<oldest_completed_target:
+            if event_time < oldest_completed_target:
                 oldest_completed_target = event_time
 
         descr = docs.get(key, '')
-        event_duration=rec.get("timing_sec",0)
-        if n_total > 1:
-            fo.write(',\n')
-        fo.write('        {"eventN":"'+escape_json_string(key) +'",\n')
-        fo.write('         "description":"' + escape_json_string(descr) + '", \n')
-        fo.write('         "eventType":"'+escape_json_string(event_type)+'",\n')
-        fo.write('         "eventTime":"'+escape_json_string(event_time)+'",\n')
-        fo.write('         "eventDuration":' + escape_json_string(event_duration) + ',\n')
-        if last_event_time == '':
-            fo.write('         "lastEventTime":' + 'null' + ',\n')
-        else:
-            fo.write('         "lastEventTime":"' + last_event_time + '",\n')
-        fo.write('         "log":"' + escape_json_string(rec["log"]) + '" \n')
+        event_duration = rec.get("timing_sec", 0)
 
-        fo.write('        }')
-    fo.write('\n')
-    fo.write('    ],\n')
+        status.append(
+            {"eventN": key,
+             "description": descr,
+             "eventType": event_type,
+             "eventTime": event_time,
+             "eventDuration": event_duration,
+             "lastEventTime": last_event_time,
+             "log": rec["log"]
+             }
+        )
 
-    if n_in_progress>0:
+    if n_in_progress > 0:
         current_status = 'Up and Running'
     else:
         current_status = 'Idle'
 
-    fo.write('    "pipeline":{ \n')
-    fo.write('        "nProgress":'+ escape_json_string(n_in_progress)+',\n')
-    fo.write('        "nEvents":'+escape_json_string(n_total)+',\n')
-    fo.write('        "nFail":'+escape_json_string(n_failed)+',\n')
-    if oldest_completed_target == '':
-        fo.write('        "oldestCompleteTime":null,\n')
-    else:
-        fo.write('        "oldestCompleteTime":"' + escape_json_string(oldest_completed_target) + '",\n')
-    fo.write('        "presentStatus": "'+current_status+'" \n')
-    fo.write('    }\n')
-    fo.write('}\n')
+    pipeline = {
+        "nProgress": n_in_progress,
+        "nEvents": n_total,
+        "nFail": n_failed,
+        "oldestCompleteTime": oldest_completed_target,
+        "presentStatus": current_status
+    }
+
+    status_list["pipeline"] = pipeline
+    status_list["status"] = status
+
+    # not sure about this!
+    fo.write(json.dumps(status_list))
+
 
     fo.close()
-
